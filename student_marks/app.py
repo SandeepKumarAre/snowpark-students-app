e3743caae0b8a552d27a4ab2337aebb7864bad3e
devopsudhir28 <devopsudhir28@gmail.com>
2025-06-07 14:42:11 +0530

    main and credentials updated2

--- student_marks/app.py
+++ student_marks/app.py
@@ -3,8 +3,10 @@ from snowflake.snowpark.functions import col
 
 def main(session: Session):
     df = session.table("students")
+    print(f"conneccted to table and shape of table {df.shape}")
 
     df_result = df.with_column("total_marks", col("subject1") + col("subject2") + col("subject3") + col("subject4")) \
                   .with_column("percentage", (col("total_marks") / 400) * 100)
-
+    print("Computations are completed")
     df_result.write.mode("overwrite").save_as_table("students_result")
+    print("Done")
